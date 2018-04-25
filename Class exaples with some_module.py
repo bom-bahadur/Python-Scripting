6a9df9f3154f0cd7d15ed47efe27217f73091f5c
Bom Bahadur Singiali <38737536+bom-bahadur@users.noreply.github.com>
2018-04-26 03:53:52 +0530

    Add files via upload

---
+++ Class exaples with some_module.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+### class example.
+
+class Foo:
+	"""my Foo Class"""
+	def __init__(self):
+		pass
+
+class Bar:
+	""" my Bar Class"""
+	def __int__(self):
+		pass
+
+class Bam:
+	"""my Bam Class"""
+	def __int__(sefl):
+		pass
+
+def baz():
+	"""my baz function"""
+	return None
